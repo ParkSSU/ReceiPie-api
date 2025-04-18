ef0ab7687d601481f58ebbf31fa52697defa021b
sangil6372 <sangil6372@naver.com>
2025-04-18 17:39:15 +0900

    init : Spring 초기 세팅

---
+++ src/main/java/com/parkssu/adari_api/AdariApiApplication.java
@@ -0,0 +1,13 @@
+package com.parkssu.adari_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdariApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdariApiApplication.class, args);
+	}
+
+}
