ab8fb131833ee6057a74a7a521b3c653fa1f249c
박상일 <sangil6372@naver.com>
2025-05-06 22:33:05 +0900

    chore: rename project to receipie

--- src/test/java/com/parkssu/receipie_api/ReceiPieApiApplicationTests.java
+++ src/test/java/com/parkssu/receipie_api/ReceiPieApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ReceiPieApiApplicationTests {
+class ReceiPieApplicationTests {
 
 	@Test
 	void contextLoads() {
