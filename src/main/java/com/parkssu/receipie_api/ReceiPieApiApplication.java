b8c2e3aeb17f3d4ae2b24034b950e04cbe1f3add
박상일 <sangil6372@naver.com>
2025-05-06 22:30:14 +0900

    chore: rename project to receipie-api

--- src/main/java/com/parkssu/receipie_api/ReceiPieApiApplication.java
+++ src/main/java/com/parkssu/receipie_api/ReceiPieApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdariApiApplication {
+public class ReceiPieApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdariApiApplication.class, args);
+		SpringApplication.run(ReceiPieApiApplication.class, args);
 	}
 
 }
